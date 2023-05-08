5879d8af6eb792ae91d0eed163f9b5756b06b986
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-08 23:55:32 +0600

    Login is added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const User = require("./models/User");
 const app = express();
 const port = 4000;
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,10 +17,26 @@ mongoose.connect(
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const userdoc = await User.create({ username, password });
-  res.json(userdoc);
+  try {
+    const userdoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userdoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
+app.get("/login", (req, res) => {
+  res.send("Got it");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username: username });
+  res.json(userDoc);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
