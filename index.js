e07a39fef24b2ebd9acf7af4be1a6ba2bd498fd1
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-05-16 13:33:19 +0600

    Show Post is added

--- index.js
+++ index.js
@@ -99,6 +99,12 @@ app.get("/post", async (req, res) => {
   );
 });
 
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
